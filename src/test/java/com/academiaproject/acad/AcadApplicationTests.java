b8f5ac550a8c9477eca27f3b1a49fc850102b8b2
filipesag <filipesag@hotmail.com>
2023-05-11 14:31:30 -0300

    project created

---
+++ src/test/java/com/academiaproject/acad/AcadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academiaproject.acad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
